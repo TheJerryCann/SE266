bff136c1a069fadb14822f1bcad9e8c580cdcf75
TheJerryCann <122393082+TheJerryCann@users.noreply.github.com>
2023-10-09 22:50:02 -0400

    Update index.view.php

--- index.view.php
+++ index.view.php
@@ -9,14 +9,33 @@
 <body>
     
     <ul>
-        <?php foreach ($animals as $animal) : ?> <!-- loops through the array of animals and prints them in a list -->
+        <?php foreach ($animals as $animal) : ?> <!-- Task C: loops through the array of animals and prints them in a list -->
             <li><?= $animal; ?></li>
         <?php endforeach; ?>
     </ul>
     <ul> 
-        <?php foreach ($tasks as $key => $task) : ?> <!-- loops through the array of tasks and keys then prints them in a list -->
+        <?php foreach ($tasks as $key => $task) : ?> <!-- Task D: loops through the array of tasks and keys then prints them in a list -->
             <li><strong><?= $key; ?></strong> <?= $task; ?></li>
         <?php endforeach; ?>
+    </ul>
+    <ul> <!-- Task E: listing the contents of the array of tasks and printing -->
+        <li>
+            <strong>Task: </strong> <?=$tasks['Title']; ?>
+        </li>
+        <li>
+            <strong>Due: </strong> <?=$tasks['Due']; ?>
+        </li>
+        <li>
+            <strong>Assigned To: </strong> <?=$tasks['Assigned_to']; ?>
+        </li>
+        <li>
+            <strong>Status: </strong> <!-- if statement for checkbox -->
+            <?php if ($tasks['Completed']) : ?>
+                <span class="icon">&#9989;</span>
+            <?php else : ?>
+                <span class="icon">Incomplete</span>
+            <?php endif; ?>
+        </li>
     </ul>
 
 </body>
