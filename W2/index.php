9f6ca66007dc461f5142592e4213d0e8aae6a294
TheJerryCann <amrea@email.neit.edu>
2023-10-16 12:31:21 -0400

    Update index.php

--- W2/index.php
+++ W2/index.php
@@ -1,14 +1,14 @@
 <?php 
 
 $patient = [
-    'First Name' => '',
+    'First Name' => document.querySelector('#fname').value,
     'Last Name' => '',
     'Married' => '',
     'Birth Date' => '',
     'Height' => '',
     'Weight' => ''
 ];
-
+console.log($patient)
 
 require 'index.view.php';
 
