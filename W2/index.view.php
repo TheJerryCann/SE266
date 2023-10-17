b307f873a50fb997a15094c03024bc88cbbc05c1
TheJerryCann <amrea@email.neit.edu>
2023-10-17 11:33:33 -0400

    Update index.view.php

--- W2/index.view.php
+++ W2/index.view.php
@@ -10,8 +10,29 @@
 <body>
     <label for="fname">First Name:</label>
     <input type="text" name="fname" required>
+    <br><br>
+    <label for="lname">Last Name:</label>
+    <input type="text" name="lname" required>
+    <br><br>
+    <label for="married">Married:</label>
+    <input type="radio" name="married" required>
+    <br><br>
+    <label for="bday">Birth Date:</label>
+    <input type="date" name="bday" required>
+    <br><br>
+    <label for="height">Height:</label>
+    <input type="number" name="height" required>
+    <br><br>
+    <label for="weight">Weight:</label>
+    <input type="number" name="weight" required>
+    <br><br>
 </body>
 <script>
     $fname = document.querySelector('#fname').value
+    $lname = document.querySelector('#lname').value
+    $married = document.querySelector('#married').value
+    $bday = document.querySelector('#bday').value
+    $height = document.querySelector('#height').value
+    $weight = document.querySelector('#weight').value
 </script>
 </html>
