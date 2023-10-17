67a306f0073c418fe4bf2a4d21507bd7ad326498
TheJerryCann <amrea@email.neit.edu>
2023-10-17 13:59:32 -0400

    Update index.php

--- W2/index.php
+++ W2/index.php
@@ -2,9 +2,14 @@
     if (isset($_POST['submit'])){
         echo "Form Submitted<hr />";
         $fname = filter_input(INPUT_POST,'fname',FILTER_VALIDATE_TEXT);
+        $lname = filter_input(INPUT_POST,'lname',FILTER_VALIDATE_TEXT);
+        $married = filter_input(INPUT_POST,'married',FILTER_VALIDATE_CHECKBOX);
+        $bday = filter_input(INPUT_POST,'bday',FILTER_VALIDATE_DATE);
+        $height = filter_input(INPUT_POST,'height',FILTER_VALIDATE_NUMBER);
+        $weight = filter_input(INPUT_POST,'weight',FILTER_VALIDATE_NUMBER);
     }
     else {
-        echo "ERROR";
+        echo "Initial Load";
     }
 ?>
 <!DOCTYPE html>
@@ -25,12 +30,12 @@
         <input type="text" name="lname" required>
         <br><br>
         <label for="married">Married:</label>
-        <input type="radio" name="married" required>
+        <input type="checkbox" name="married" required>
         <br><br>
         <label for="bday">Birth Date:</label>
         <input type="date" name="bday" required>
         <br><br>
-        <label for="height">Height:</label>
+        <label for="height">Height(in):</label>
         <input type="number" name="height" required>
         <br><br>
         <label for="weight">Weight:</label>
