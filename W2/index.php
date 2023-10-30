951fb047fd85328642e2e8344a5b3e11ecded634
TheJerryCann <122393082+TheJerryCann@users.noreply.github.com>
2023-10-29 21:24:41 -0400

    Update index.php

--- W2/index.php
+++ W2/index.php
@@ -11,25 +11,25 @@
 <body>
     <form action="index.php" method="post"> <!-- All inputs for patient -->
         <label for="fname">First Name:</label>
-        <input type="text" name="fname" value="<?= $fname; ?>" required>
+        <input type="text" name="fname" required>
         <br><br>
         <label for="lname">Last Name:</label>
-        <input type="text" name="lname" value="<?= $lname; ?>" required>
+        <input type="text" name="lname" required>
         <br><br>
         <label for="married">Married:</label>
-        <input type="checkbox" name="married" value="<?= $married; ?>">
+        <input type="checkbox" name="married">
         <br><br>
         <label for="bday">Birth Date:</label>
-        <input type="date" name="bday" value="<?= $bday; ?>" required>
+        <input type="date" name="bday" required>
         <br><br>
         <label for="heightFT">Height(ft):</label>
-        <input type="number" name="heightFT" value="" required>
+        <input type="number" name="heightFT" required>
         <br><br>
         <label for="heightIN">Height(in):</label>
-        <input type="number" name="heightIN" value="" required>
+        <input type="number" name="heightIN" required>
         <br><br>
         <label for="weight">Weight:</label>
-        <input type="number" name="weight" value="<?= $weight; ?>" required>
+        <input type="number" name="weight" required>
         <br><br>
         <input type="submit" name="submitBtn"/> <!-- Submit Button -->
         </form>
