c519f755d98e7ffa49b1081b122e672e91067085
TheJerryCann <amrea@email.neit.edu>
2023-10-30 12:52:23 -0400

    Update account.php

--- W3/account.php
+++ W3/account.php
@@ -17,11 +17,18 @@ abstract class Account
 	public function __construct ($id, $bal, $startDt) 
 	{
 	   // write code here
+	   $this->accountId = $id;
+	   $this->balance = $bal;
+	   $this->startDate = $startDt;
 	} // end constructor
 	
 	public function deposit ($amount) 
 	{
 		// write code here
+		if ($amount > 0) {
+			$this->balance += $amount
+			return true;
+		}
 	} // end deposit
 
 	// This is an abstract method. 
@@ -32,22 +39,30 @@ abstract class Account
 	public function getStartDate() 
 	{
 		// write code here
+		return $this->startDate;
 	} // end getStartDate
 
 	public function getBalance() 
 	{
 		// write code here
+		return $this->balance;
 	} // end getBalance
 
 	public function getAccountId() 
 	{
 		// write code here
+		return $this->accountId;
 	} // end getAccountId
 
 	// Display AccountID, Balance and StartDate in a nice format
 	protected function getAccountDetails()
 	{
 		// write code here
+		$accountDetails = "Account ID: $this->accountId <br>";
+		$accountDetails = "Balance: $this->balance <br>";
+		$accountDetails = "Start Date: $this->startDate <br>";
+
+		return $accountDetails;
 	} // end getAccountDetails
 	
 } // end account
