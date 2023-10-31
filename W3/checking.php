7930f503a6f247550575e8f0bbe4a45cce49918a
TheJerryCann <amrea@email.neit.edu>
2023-10-31 13:05:55 -0400

    Update checking.php

--- W3/checking.php
+++ W3/checking.php
@@ -10,7 +10,7 @@ class CheckingAccount extends Account
 	{
 		// write code here. Return true if withdrawal goes through; false otherwise
 		if ($amount > 0 && ($this->balance - $amount >= self::OVERDRAW_LIMIT)){
-			$this->balance -> $amount;
+			$this->balance -= $amount;
 			return true;
 		}
 		return false;
@@ -30,11 +30,11 @@ class CheckingAccount extends Account
 // The code below runs everytime this class loads and 
 // should be commented out after testing.
 
-$checking = new CheckingAccount ('C123', 1000, '12-20-2019');
+/*$checking = new CheckingAccount ('C123', 1000, '12-20-2019');
 $checking->withdrawal(200);
 $checking->deposit(500);
 
 echo $checking->getAccountDetails();
-echo $checking->getStartDate();
+echo $checking->getStartDate();*/	
     
 ?>
