8123a17835eefce7904b16f25ad7916dcc51cb02
TheJerryCann <122393082+TheJerryCann@users.noreply.github.com>
2023-11-25 20:22:54 -0500

    Create model_patients.php

---
+++ W4/model_patients.php
@@ -0,0 +1,32 @@
+<?php
+
+    include(__DIR__ . '/db.php');
+
+    function getPatients () {
+        global $db;
+        $results = [];
+        $stmt = $db->prepare("SELECT id, fname, lname, married, bday FROM patients ORDER BY lname");
+
+        if ( $stmt->execute() && $stmt->rowcount() > 0 ) {
+            $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+        }
+        return($results);
+    }
+
+    function addPatient ($f, $l, $m, $b) {
+        global $db;
+        $stmt = $db->prepare("INSERT INTO patients SET fname = :fname, lname = :lname, married = :married, bday = :bday");
+        $binds = array(
+            ":fname" => $f,
+            ":lname" => $l,
+            ":married" => $m,
+            ":bday" => $b
+        );
+        if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
+            $results = 'Data Added';
+        }
+        return ($results);
+    }
+    $patients = getPatients();
+
+?>
